12940fc71c228ef9e4fa5922a33f507b311ba290
Konstantin K <67804350+qeiynn@users.noreply.github.com>
2020-08-05 19:02:18 +0200

    Update listen-for-shutdown.py

--- listen-for-shutdown.py
+++ listen-for-shutdown.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 
-
 import RPi.GPIO as GPIO
 import subprocess
-
+from time import sleep
 
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.wait_for_edge(3, GPIO.FALLING)
 
-subprocess.call(['shutdown', '-h', 'now'], shell=False)
+#wait for 2 seconds to see if this was deliberate
+sleep(2)
+# check the button level again
+if GPIO.input(3) == 0:
+    subprocess.call(['shutdown', '-h', 'now'], shell=False)
