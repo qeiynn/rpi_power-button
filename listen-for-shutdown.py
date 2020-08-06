0b33727a2a012ab4491de205c2e68e6cd508c247
Konstantin K <67804350+qeiynn@users.noreply.github.com>
2020-08-06 16:54:49 +0200

    Update listen-for-shutdown.py

--- listen-for-shutdown.py
+++ listen-for-shutdown.py
@@ -9,7 +9,7 @@ GPIO.setwarnings(False)
 GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.wait_for_edge(3, GPIO.FALLING)
 
-#wait for 2 seconds to see if this was deliberate
+#wait for 2 seconds in case button press was accidental
 sleep(2)
 # check the button level again
 if GPIO.input(3) == 0:
