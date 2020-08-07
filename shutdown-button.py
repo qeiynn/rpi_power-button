0365f5f6cb58e4a603d87f8618fcedfe180c0f1f
Konstantin K <67804350+qeiynn@users.noreply.github.com>
2020-08-07 08:27:00 +0200

    Update shutdown-button.py

--- shutdown-button.py
+++ shutdown-button.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python
 
 import RPi.GPIO as GPIO
-import subprocess
+import os
+#import subprocess
 from time import sleep
+
+gpio_pin_number=3
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.wait_for_edge(3, GPIO.FALLING)
+GPIO.setup(gpio_pin_number, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-#wait for 2 seconds in case button press was accidental
-sleep(2)
-# check the button level again
-if GPIO.input(3) == 0:
-    subprocess.call(['shutdown', '-h', 'now'], shell=False)
+while True:
+    GPIO.wait_for_edge(gpio_pin_number, GPIO.FALLING)
+    #sleep and check button again (in case of accidental button press)
+    time.sleep(2)
+    if GPIO.input(gpio_pin_number) == 0:
+        break
+        
+print("ta ta")
+time.sleep(1)
+os.system("sudo shutdown -h now")
