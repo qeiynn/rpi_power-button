6b0c87e668fa413921a393fc6a7e014c598f9b00
Konstantin K <67804350+qeiynn@users.noreply.github.com>
2020-08-07 12:23:41 +0200

    Update shutdown-button.sh

--- shutdown-button.sh
+++ shutdown-button.sh
@@ -5,5 +5,5 @@ sudo systemctl stop hostap &&
 #comment lines 61-63
 sudo sed -i '61,63 s/^/#/' /etc/dhcpcd.conf
 
-#uncomment lines 61-63
+#uncomment lines 61-63 WARNING: ALSO REMOVES SPACES
 sudo sed -i "61,63 s/# *//" /etc/dhcpcd.conf
