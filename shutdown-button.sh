2a60725c0496bf751c6bc6f3c6f488202d0321f3
Konstantin K <67804350+qeiynn@users.noreply.github.com>
2020-08-07 11:40:36 +0200

    Update shutdown-button.sh

--- shutdown-button.sh
+++ shutdown-button.sh
@@ -1,29 +1,9 @@
 #! /bin/sh
 
-### BEGIN INIT INFO
-# Provides:          listen-for-shutdown.py
-# Required-Start:    $remote_fs $syslog
-# Required-Stop:     $remote_fs $syslog
-# Default-Start:     2 3 4 5
-# Default-Stop:      0 1 6
-### END INIT INFO
+sudo systemctl stop hostap &&
 
-# If you want a command to always run, put it here
+#comment lines 61-63
+sudo sed -i '61,63 s/^/#/' /etc/dhcpcd.conf
 
-# Carry out specific functions when asked to by the system
-case "$1" in
-  start)
-    echo "Actiavting power-button script"
-    /usr/local/bin/shutdown-button.py &
-    ;;
-  stop)
-    echo "Stopping listen-for-shutdown.py"
-    pkill -f /usr/local/bin/shutdown-button.py
-    ;;
-  *)
-    echo "Usage: /etc/init.d/shutdown-button.sh {start|stop}"
-    exit 1
-    ;;
-esac
-
-exit 0
+#uncomment lines 61-63
+sudo sed -i "61,63 s/# *//" /etc/dhcpcd.conf
