3b2ba03ae03762397082397a6956f96c7b855b07
Konstantin K <67804350+qeiynn@users.noreply.github.com>
2020-08-10 14:18:15 +0200

    Update shutdown-button.py

--- shutdown-button.py
+++ shutdown-button.py
@@ -4,7 +4,6 @@ import RPi.GPIO as GPIO
 import time
 import subprocess
 
-gpio_pin_number=3
 def Shutdown():
     print("Cowabunga!")
     time.sleep(1)
@@ -12,12 +11,12 @@ def Shutdown():
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(gpio_pin_number, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
-    GPIO.wait_for_edge(gpio_pin_number, GPIO.FALLING)
+    GPIO.wait_for_edge(3, GPIO.FALLING)
     #sleep and check button again (in case of accidental button press)
     time.sleep(2)
-    if GPIO.input(gpio_pin_number) == 0:
+    if GPIO.input(3) == 0:
         break
 Shutdown()
