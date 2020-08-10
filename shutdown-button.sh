9e7b1bc9d83aac71f52f74310b1d288a70ed85be
Konstantin K <67804350+qeiynn@users.noreply.github.com>
2020-08-10 14:22:02 +0200

    Update shutdown-button.sh

--- shutdown-button.sh
+++ shutdown-button.sh
@@ -1,5 +1,13 @@
 #! /bin/sh
 
+### BEGIN INIT INFO
+# Provides:          shutdown-button.py
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+### END INIT INFO
+
 case "$1" in
   start)
     echo "Activating Power-Button script"
